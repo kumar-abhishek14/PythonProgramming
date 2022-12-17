377921c7fe5e1654cd92ea8eceb9f1d9c5d67442
kumar.abhishek14 <kumar.abhishek14@outlok.com>
2022-12-17 17:56:53 +0530

    Accessing an item of list

---
+++ List_in_Python/accessing_an_item.py
@@ -0,0 +1,8 @@
+list=["Ravi","Ghanshyam","Abhishek","Mohan"]
+#list item are accessed through index starting with 0
+#accessing first element
+print(list[0])
+
+#accessing all item of list
+for x in list:
+    print(x)
