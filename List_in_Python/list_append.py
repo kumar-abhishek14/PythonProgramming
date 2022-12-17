4f0f74ddf54f0e9b1fe6e6baef26d23296158d10
kumar.abhishek14 <kumar.abhishek14@outlok.com>
2022-12-17 17:51:02 +0530

    Adding an item to list

---
+++ List_in_Python/list_append.py
@@ -0,0 +1,10 @@
+list=["Ravi","Akash","Ramesh"]
+print("Before adding an item to list:-",list)
+list.append("Shyam")
+print("After adding an new item to list",list)
+#Adding an item at an index
+
+list.insert(3,"Mohan")
+print("After adding element at index 3",list)
+
+
